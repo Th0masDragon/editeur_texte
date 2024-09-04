4322b47084d5f5853dc4dc3f6e5ef6e491aeb78c
Thomas Julien-Servel <thomas.julien-servel@etu.umontpellier.fr>
2024-09-04 15:33:53 +0200

    ajout de commande remplacer, à completeer
    bon sa rempalcer comme il faut

---
+++ src/main/java/fr/iut/editeur/commande/CommandeRemplacer.java
@@ -0,0 +1,22 @@
+package fr.iut.editeur.commande;
+
+import fr.iut.editeur.document.Document;
+
+public class CommandeRemplacer extends CommandeDocument {
+
+
+    public CommandeRemplacer(Document document, String[] parameters) {
+        super(document, parameters);
+    }
+
+    @Override
+    public void executer() {
+        if(parameters.length < 3) {
+            System.err.println("Format attendu : remplacer;debut;fin;chaine");
+            return;
+        }
+        this.document.remplacer(Integer.parseInt(parameters[1]), Integer.parseInt(parameters[2]), parameters[3]);
+        super.executer();
+    }
+
+}
