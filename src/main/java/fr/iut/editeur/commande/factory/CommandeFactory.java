ede3d2bd58643f687763c2c4c41cd942df2da88a
Thomas Julien-Servel <thomas.julien-servel@etu.umontpellier.fr>
2024-09-06 17:20:56 +0200

    Closes #1    fix de clear

--- src/main/java/fr/iut/editeur/commande/factory/CommandeFactory.java
+++ src/main/java/fr/iut/editeur/commande/factory/CommandeFactory.java
@@ -22,7 +22,7 @@ public class CommandeFactory {
             case "remplacer" : return new CommandeRemplacer(document, parameters);
             case "majuscules" : return new CommandeMajuscules(document, parameters);
             case "effacer" : return new CommandeEffacer(document, parameters);
-            case "cleareeeuuuuu" : return new CommandeEffacer(document, parameters);
+            case "clear" : return new CommandeEffacer(document, parameters);
             default: return null;
         }
     }
